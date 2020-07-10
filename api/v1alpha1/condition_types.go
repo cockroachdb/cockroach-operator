bbfc23a928c1c1d39f167057ac814756a1c5d00f
Vlad Artamonov <742047+vladdy@users.noreply.github.com>
2020-07-10 16:39:08 -0400

    Document CR fields (#49)

--- api/v1alpha1/condition_types.go
+++ api/v1alpha1/condition_types.go
@@ -13,11 +13,16 @@ const (
 // TODO(vladdy): this should eventually be converged with
 //     https://github.com/kubernetes/enhancements/pull/1624
 
+// ClusterCondition represents cluster status as it is perceived by
+// the operator
 type ClusterCondition struct {
 	// +required
+	// Type/Name of the condition
 	Type ClusterConditionType `json:"type"`
 	// +required
+	// Condition status: True, False or Unknown
 	Status metav1.ConditionStatus `json:"status"`
 	// +required
+	// The time when the condition was updated
 	LastTransitionTime metav1.Time `json:"lastTransitionTime"`
 }
