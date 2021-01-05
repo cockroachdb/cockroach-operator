7e0a16c1046cf780a159263a37fe8e8596077584
chrislovecnm <clove@cnmconsulting.net>
2021-01-05 17:10:18 +0000

    Updating Year to 2021

--- hack/bin/tools.go
+++ hack/bin/tools.go
@@ -1,7 +1,7 @@
 // +build tools
 
 /*
-Copyright 2020 The Cockroach Authors
+Copyright 2021 The Cockroach Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
