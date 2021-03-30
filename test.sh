f9f5283441cd19acba3e06afc385f78d5cb12ac8
John Kendall <john@cockroachlabs.com>
2021-03-30 14:45:52 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-# This script is for installing the Operator in development
-# Bazel has go rules that require root access because of Big Sur (Mac OS).
+# This script installs the Operator in development.
+# Bazel has go rules that require root access because of Big Sur (macOS).
 # When running with sudo/root access, it opens a new shell, so the environment variables are not used with the existing Developer Install Instructions (https://github.com/cockroachdb/cockroach-operator/blob/master/DEVELOPER.md#developer-install-instructions).
 # This script injects the required environment variables to allow the "make k8s/apply" command to succeed.
 sudo \
