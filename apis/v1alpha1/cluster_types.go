f93a9482b3da036880b5f1dfd477d0dfc26f69ec
Alina <alinadonisa@users.noreply.github.com>
2021-04-19 20:55:51 +0300

    remove empty dir (#420)
    Co-authored-by: Chris Love <clove@cnmconsulting.net>

--- apis/v1alpha1/cluster_types.go
+++ apis/v1alpha1/cluster_types.go
@@ -205,9 +205,6 @@ type Volume struct {
 	// Directory from the host node's filesystem
 	// +optional
 	HostPath *corev1.HostPathVolumeSource `json:"hostPath,omitempty"`
-	// Temporary folder on the host node's filesystem
-	// +optional
-	EmptyDir *corev1.EmptyDirVolumeSource `json:"emptyDir,omitempty"`
 	// Persistent volume to use
 	// +optional
 	VolumeClaim *VolumeClaim `json:"pvc,omitempty"`
