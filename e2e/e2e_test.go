7885d126a91514c53a09e2d5470fa598c3a49c7e
alina.donisa <alina.vieriu@gmail.com>
2021-05-24 20:40:06 +0300

    change tests

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -177,7 +177,7 @@ func TestUpgradesMinorVersion(t *testing.T) {
 	steps.Run(t)
 }
 
-func TestUpgradesMajorVersion19to20(t *testing.T) {
+func TestUpgradesMajorVersion20to21(t *testing.T) {
 
 	// We are doing a major version upgrade here
 	// 19 to 20
@@ -197,7 +197,7 @@ func TestUpgradesMajorVersion19to20(t *testing.T) {
 	sb.StartManager(t, controller.InitClusterReconcilerWithLogger(testLog))
 
 	builder := testutil.NewBuilder("crdb").WithNodeCount(3).WithTLS().
-		WithImage("cockroachdb/cockroach:v19.2.6").
+		WithImage("cockroachdb/cockroach:v20.2.9").
 		WithPVDataStore("1Gi", "standard" /* default storage class in KIND */)
 
 	steps := Steps{
@@ -215,7 +215,7 @@ func TestUpgradesMajorVersion19to20(t *testing.T) {
 				current := builder.Cr()
 				require.NoError(t, sb.Get(current))
 
-				current.Spec.Image.Name = "cockroachdb/cockroach:v20.1.1"
+				current.Spec.Image.Name = "cockroachdb/cockroach:v21.1.0"
 				require.NoError(t, sb.Update(current))
 
 				RequireClusterToBeReadyEventuallyTimeout(t, sb, builder, 500*time.Second)
