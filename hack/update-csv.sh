4353376cc1688684343286a8b3e8db37ea44fad3
Rail Aliiev <rail@iqchoice.com>
2021-05-28 11:45:04 -0400

    Drop 19.2.x support, centralize supported crdb versions
    * Drop 19.2.12 support.
    * Centralize list of supported CRDB versions used by some scripts

--- hack/update-csv.sh
+++ hack/update-csv.sh
@@ -54,24 +54,23 @@ echo "RH_BUNDLE_METADATA_OPTS=$RH_BUNDLE_METADATA_OPTS"
 [[ -z "$9" ]] && { echo "Error: RH_COCKROACH_DATABASE_IMAGE not set"; exit 1; }
 RH_COCKROACH_DATABASE_IMAGE="$9"
 echo "RH_COCKROACH_DATABASE_IMAGE=$RH_COCKROACH_DATABASE_IMAGE"
-SUPPORTED_VERSIONS=(v20.2.5 v20.1.12 v19.2.12)
-"$opsdk" generate kustomize manifests -q 
+"$opsdk" generate kustomize manifests -q
 "$kstomize" build config/manifests | "$opsdk" generate bundle -q --overwrite --version ${RH_BUNDLE_VERSION} ${RH_BUNDLE_METADATA_OPTS}
 "$opsdk" bundle validate ./bundle
 
 VAR_VERSIONS=""
-for v in "${SUPPORTED_VERSIONS[@]}"
+for v in $(grep -v '#' ${REPO_ROOT}/crdb-versions.txt)
 do
   vrs=${v//./_}
   ENV_VAR_PLACEHOLDER="RH_COCKROACH_DB_IMAGE_PLACEHOLDER_${vrs}"
   RH_COCKROACH_DATABASE_IMAGE_VERSION="registry.connect.redhat.com/cockroachdb/cockroach:${v}"
   VAR_VERSIONS+="s+${ENV_VAR_PLACEHOLDER}+${RH_COCKROACH_DATABASE_IMAGE_VERSION}+g; "
-done 
-cat bundle/manifests/cockroach-operator.clusterserviceversion.yaml | sed "${VAR_VERSIONS}s+RH_COCKROACH_OP_IMAGE_PLACEHOLDER+${RH_COCKROACH_OP_IMG}+g; s+CREATED_AT_PLACEHOLDER+"$(date +"%FT%H:%M:%SZ")"+g"> bundle/manifests/cockroach-operator.clusterserviceversion.yaml 
+done
+cat bundle/manifests/cockroach-operator.clusterserviceversion.yaml | sed "${VAR_VERSIONS}s+RH_COCKROACH_OP_IMAGE_PLACEHOLDER+${RH_COCKROACH_OP_IMG}+g; s+CREATED_AT_PLACEHOLDER+"$(date +"%FT%H:%M:%SZ")"+g"> bundle/manifests/cockroach-operator.clusterserviceversion.yaml
 cd  bundle/manifests && "$faq" -f yaml -o yaml --slurp '.[0].spec.install.spec.clusterPermissions+= [{serviceAccountName: .[2].metadata.name, rules: .[1].rules }] | .[0]' cockroach-operator.clusterserviceversion.yaml cockroach-database-role_rbac.authorization.k8s.io_v1_clusterrole.yaml cockroach-database-sa_v1_serviceaccount.yaml > csv.yaml
-mv csv.yaml cockroach-operator.clusterserviceversion.yaml 
+mv csv.yaml cockroach-operator.clusterserviceversion.yaml
 shopt -s extglob
-rm -v !("cockroach-operator.clusterserviceversion.yaml"|"crdb.cockroachlabs.com_crdbclusters.yaml") 
+rm -v !("cockroach-operator.clusterserviceversion.yaml"|"crdb.cockroachlabs.com_crdbclusters.yaml")
 shopt -u extglob
 cd ${REPO_ROOT}
 FILE_NAMES=(bundle/tests/scorecard/config.yaml \
@@ -85,13 +84,13 @@ do
    :
    cat "${REPO_ROOT}/hack/boilerplate/boilerplate.yaml.txt" "${REPO_ROOT}/${YAML}" > "${REPO_ROOT}/${YAML}.mod"
    mv "${REPO_ROOT}/${YAML}.mod" "${REPO_ROOT}/${YAML}"
-done 
+done
 
 DOCKER_FILE_NAME="bundle.Dockerfile"
 
 cat "${REPO_ROOT}/hack/boilerplate/boilerplate.Dockerfile.txt" "${REPO_ROOT}/${DOCKER_FILE_NAME}" > "${REPO_ROOT}/${DOCKER_FILE_NAME}.mod"
 mv "${REPO_ROOT}/${DOCKER_FILE_NAME}.mod" "${REPO_ROOT}/${DOCKER_FILE_NAME}"
- 
+
 
 
 
