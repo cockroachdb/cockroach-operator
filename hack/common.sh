b7ffc7e856d881ea0a7a88ed42daef2992628cfb
David Muto (pseudomuto) <david.muto@gmail.com>
2021-09-10 17:10:53 -0400

    Dropping dev/run in favour of up/down
    Currently, we've got the ability to run the operator locally using `make
    dev/run`. This starts a kind cluster and runs the operator in the
    foreground.
    
    When we add webhooks for the admission controller (in progress), we'll
    need to ensure that the operator is deployed to the cluster rather than
    running in the foreground on the local host. This is necessary because
    the service will need to be able to use a selector (matchLabels) to find
    the pod that's running the webhook server.
    
    This change adds `make dev/up` and `make dev/down` which start and stop
    the kind cluster respectively. When running `dev/up`, the image is
    published to gcr.io (`$DEV_REGISTRY`) and kind is configured with
    permissions to pull from there.

--- hack/common.sh
+++ hack/common.sh
@@ -20,6 +20,9 @@
 # "-                                                       -"
 # "---------------------------------------------------------"
 
+# load env vars from rc file if it exists
+[ -f .envrc ] && export $(cat .envrc | sed 's/#.*//g' | xargs)
+
 # gcloud and kubectl are required for this POC
 command -v gcloud >/dev/null 2>&1 || { \
  echo >&2 "I require gcloud but it's not installed.  Aborting."; exit 1; }
