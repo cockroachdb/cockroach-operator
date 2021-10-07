a1641c657963ec88c0c9004d29f71ed54bc4b969
David Muto (pseudomuto) <david.muto@gmail.com>
2021-10-07 10:42:05 -0400

    Add nightly tests for supported K8s versions
    We need to ensure that we're testing all of the supported versions of
    Kubernetes. As APIs are upgraded/removed we should not rely on manually
    verifying which APIs are being used and which are still valid.
    
    I've added a smoke test here which stands up a kind cluster (for each
    supported version), deploys the operator, and runs the bank workload for
    a few seconds just to ensure things are working.
    
    You might be thinking, "why not just run (one of) our e2e tests?" The
    answer is that I wanted to ensure we had a test in place that does what
    our [docs] tell users to do. Which is to install the crds from
    _install/crds.yaml_, the operator from _install/operator.yaml_, and the
    example from _examples/example.yaml_. These files are updated whenever
    we do a release or run `make release/gen-templates`.
    
    To make sure the cluster is functional, the test also adds the secure
    client pod and executes the [bank workload] for 10s.
    
    Finally, I've added a new GitHub workflow to run these tests nightly. I
    chose 3am rather arbitrarily since I figured almost everyone else
    chooses midnight :smile:
    
    [docs]:
    https://www.cockroachlabs.com/docs/stable/deploy-cockroachdb-with-kubernetes.html#step-2-start-cockroachdb
    [bank workload]:
    https://www.cockroachlabs.com/docs/stable/cockroach-workload.html#run-the-bank-workload

---
+++ hack/smoketest/main.go
@@ -0,0 +1,89 @@
+/*
+Copyright 2021 The Cockroach Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"sigs.k8s.io/kubetest2/pkg/process"
+)
+
+var (
+	clusterName string
+	dir         string
+	version     string
+)
+
+// main runs a simple smoke test that ensures that the operator brings up the database and that it's all functioning as
+// expected.
+//
+// It roughly follows the steps we provide in our docs:
+// https://www.cockroachlabs.com/docs/stable/deploy-cockroachdb-with-kubernetes.html#step-2-start-cockroachdb
+//
+// It will:
+// * Start a kind cluster
+// * Install the CRDs and the operator from the install dir
+// * Apply the example cluster
+// * Add the client-secure-operator
+// * Run the bank workload
+func main() {
+	flag.StringVar(&clusterName, "cluster", "smoketest", "the name of the kind cluster")
+	flag.StringVar(&dir, "dir", ".", "the directory run in")
+	flag.StringVar(&version, "version", "1.22.1", "the version of kubernetes (kindest node)")
+	flag.Parse()
+
+	// ensure kind, kubectl, etc. are on the path
+	path := os.Getenv("PATH")
+	os.Setenv("PATH", fmt.Sprintf("%s:%s", filepath.Join(os.Getenv("PWD"), "hack", "bin"), path))
+
+	// change to the desired dir (typically $BUILD_WORKSPACE_DIRECTORY)
+	if err := os.Chdir(dir); err != nil {
+		bail(err)
+	}
+
+	steps := []Step{
+		StartKindCluster(clusterName, version),
+		ApplyManifest(filepath.Join("install", "crds.yaml")),
+		ApplyManifest(filepath.Join("install", "operator.yaml")),
+		WaitForDeploymentAvailable("cockroach-operator"),
+		WaitForSecret("cockroach-operator-webhook-tls"),
+		ApplyManifest(filepath.Join("examples", "example.yaml")),
+		WaitForStatefulSetRollout("cockroachdb"),
+		ApplyManifest(filepath.Join("examples", "client-secure-operator.yaml")),
+		WaitForPodReady("cockroachdb-client-secure"),
+		InitBankWorkload(),
+		RunBankWorkload(10 * time.Second),
+	}
+
+	defer StopKindCluster(clusterName).Apply(process.ExecJUnit)
+
+	for _, step := range steps {
+		if err := step.Apply(process.ExecJUnit); err != nil {
+			bail(err)
+		}
+	}
+}
+
+func bail(err error) {
+	fmt.Fprintf(os.Stderr, "OOPS! An error occurred: %s\n", err)
+	StopKindCluster(clusterName).Apply(process.ExecJUnit)
+	os.Exit(1)
+}
