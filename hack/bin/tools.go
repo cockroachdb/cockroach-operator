ab1f129e1b3ab3986431c02aa16c0503705e981c
Prafull Ladha <prafull.ladha@gmail.com>
2025-01-23 21:24:17 +0530

    Update license to 2025 (#1076)

--- hack/bin/tools.go
+++ hack/bin/tools.go
@@ -1,7 +1,7 @@
 // +build tools
 
 /*
-Copyright 2024 The Cockroach Authors
+Copyright 2025 The Cockroach Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
